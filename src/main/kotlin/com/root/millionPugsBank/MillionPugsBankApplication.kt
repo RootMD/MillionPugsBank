525a735bfdaa4e62ff7d1c254a2779b624e36fb5
Michal Rutkowski <Fosternmr@gmail.com>
2022-02-19 19:56:32 +0100

    init blank project from initializr

---
+++ src/main/kotlin/com/root/millionPugsBank/MillionPugsBankApplication.kt
@@ -0,0 +1,11 @@
+package com.root.millionPugsBank
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MillionPugsBankApplication
+
+fun main(args: Array<String>) {
+	runApplication<MillionPugsBankApplication>(*args)
+}
---
+++ src/test/kotlin/com/root/millionPugsBank/MillionPugsBankApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.root.millionPugsBank
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MillionPugsBankApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
