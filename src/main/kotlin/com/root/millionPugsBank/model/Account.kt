24093de17955ec30192db312f61fcb431ac9afac
Michal Rutkowski <Fosternmr@gmail.com>
2022-02-20 17:26:55 +0100

    Closes #7 Add H2 Annotations for models and configuration

--- src/main/kotlin/com/root/millionPugsBank/model/Account.kt
+++ src/main/kotlin/com/root/millionPugsBank/model/Account.kt
@@ -1,7 +1,16 @@
 package com.root.millionPugsBank.model
 
+import javax.persistence.*
+
+@Entity
+@Table(name = "account")
 data class Account(
-    val ID: Long,
     val balance: Double = 0.0,
-    val User: User
-)
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    val user: User
+) {
+    @Id
+    @GeneratedValue
+    var accountId: Long = 0
+}
--- src/main/kotlin/com/root/millionPugsBank/model/User.kt
+++ src/main/kotlin/com/root/millionPugsBank/model/User.kt
@@ -1,13 +1,20 @@
 package com.root.millionPugsBank.model
 
+import javax.persistence.*
+
+@Entity
+@Table(name = "user")
 data class User(
-    val id: Long,
     val firstName: String,
     val lastName: String,
     val loginID: String,
     val password: String,
+    @OneToMany(mappedBy = "user")
     val accountList: MutableList<Account> = ArrayList()
 ) {
+    @Id
+    @GeneratedValue
+    val userId: Long = 0
     var email: String = ""
     var address: String = ""
 }
