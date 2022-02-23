4cf56a9f9dc422743a7ed53910899035e55f7365
Michal Rutkowski <Fosternmr@gmail.com>
2022-02-23 14:59:02 +0100

    Closes #12 Added JsonIgnoreProperties for Looping bug and added column labels for db cleancode

--- src/main/kotlin/com/root/millionPugsBank/model/Account.kt
+++ src/main/kotlin/com/root/millionPugsBank/model/Account.kt
@@ -1,13 +1,16 @@
 package com.root.millionPugsBank.model
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import javax.persistence.*
 
 @Entity
 @Table(name = "account")
 data class Account(
+    @Column(name = "balance")
     val balance: Double = 0.0,
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnoreProperties("accountList")
     val user: User
 ) : BaseEntity() {
 }
--- src/main/kotlin/com/root/millionPugsBank/model/User.kt
+++ src/main/kotlin/com/root/millionPugsBank/model/User.kt
@@ -1,17 +1,26 @@
 package com.root.millionPugsBank.model
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import javax.persistence.*
 
 @Entity
 @Table(name = "user")
 data class User(
+    @Column(name = "first_name")
     val firstName: String,
+    @Column(name = "last_name")
     val lastName: String,
+    @Column(name = "login_id")
     val loginID: String,
-    val password: String,
-    @OneToMany(cascade = [CascadeType.ALL], mappedBy = "user")
-    val accountList: MutableSet<Account> = HashSet()
+    @Column(name = "password")
+    val password: String
 ) : BaseEntity() {
+    @Column(name = "account_list")
+    @OneToMany(cascade = [CascadeType.ALL], mappedBy = "user")
+    @JsonIgnoreProperties("user")
+    val accountList: MutableSet<Account> = mutableSetOf()
+    @Column(name = "email")
     var email: String = ""
+    @Column(name = "address")
     var address: String = ""
 }
