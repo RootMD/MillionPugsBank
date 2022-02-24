0b4b9f81f56da8f7d556991ba664cf18b114cfe0
Michal Rutkowski <Fosternmr@gmail.com>
2022-02-25 00:47:30 +0100

    Closes #2 Controller for User done

---
+++ src/main/kotlin/com/root/millionPugsBank/controller/UserController.kt
@@ -0,0 +1,47 @@
+package com.root.millionPugsBank.controller
+
+import com.root.millionPugsBank.model.Account
+import com.root.millionPugsBank.model.User
+import com.root.millionPugsBank.service.crud.AccountService
+import com.root.millionPugsBank.service.crud.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/user")
+class UserController(
+    val userService: UserService,
+    val accountService: AccountService
+) {
+
+    @ExceptionHandler(NoSuchElementException::class)
+    fun handleNotFound(e: NoSuchElementException): ResponseEntity<String> =
+        ResponseEntity(e.message, HttpStatus.NOT_FOUND)
+
+    @GetMapping
+    fun getUsers(): Collection<User> = userService.findAll()
+
+    @GetMapping("/{id}")
+    fun getUser(@PathVariable id: Long): User {
+        return userService.findById(id)
+    }
+
+    @PostMapping
+    fun addUser(@RequestBody user: User): User {
+        val createdUser = userService.save(user)
+        accountService.save(Account(user = createdUser))
+        return createdUser
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteUser(@PathVariable id: Long) {
+        userService.deleteByID(id)
+    }
+
+    @PatchMapping
+    fun updateUser(@RequestBody user: User): User {
+        return userService.updateUser(user)
+    }
+
+}
